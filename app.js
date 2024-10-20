e5ef2abebd36447db8eb9223a1580ec42f89b38c
Michal Gawik <gawlik.zator@o2.pl>
2024-10-20 07:30:06 +0200

    use routes

--- app.js
+++ app.js
@@ -4,15 +4,21 @@ require('express-async-errors')
 const express = require('express')
 const app = express()
 
+// connect to DB
+
+// routes
+const authRouter = require('./routes/auth-route')
+const jobsRouter = require('./routes/jobs-route')
+
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
 app.use(express.json())
 // extra packages
 
-app.get('/', (req, res) => {
-	res.send('jobs api')
-})
+// routes
+app.use('/api/v1/auth', authRouter)
+app.use('domain/api/v1/jobs', jobsRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
