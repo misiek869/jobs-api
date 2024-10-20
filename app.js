35ddbdefe492a43d2171b5c35bd0cba80dae2bff
Michal Gawik <gawlik.zator@o2.pl>
2024-10-20 06:50:23 +0200

    update

--- app.js
+++ app.js
@@ -6,3 +6,27 @@ const app = express
 
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
+
+app.use(express.json())
+// extra packages
+
+app.get('/', (req, res) => {
+	res.send('jobs api')
+})
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+	try {
+		app.listen(port, () => {
+			console.log(`Server is listening on port ${port}...`)
+		})
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+start()
