058b74821dd18d8d34aef4d02cd4c8d9595f7574
Michal Gawik <gawlik.zator@o2.pl>
2024-10-21 12:02:05 +0200

    export UnauthenticatedError

--- errors/index.js
+++ errors/index.js
@@ -1,7 +1,9 @@
 const CustomAPIError = require('./custom-api-error')
 const BadRequestError = require('./bad-request-error')
+const UnauthenticatedError = require('./unauthenticated-error')
 
 module.exports = {
 	CustomAPIError,
 	BadRequestError,
+	UnauthenticatedError,
 }
