9f04b5b043202fa92d50c9ee7614ac5890adb126
Michal Gawik <gawlik.zator@o2.pl>
2024-10-21 14:24:26 +0200

    create JobSchema

---
+++ models/job-model.js
@@ -0,0 +1,32 @@
+const { required } = require('joi')
+const mongoose = require('mongoose')
+
+const JobSchema = new mongoose.Schema(
+	{
+		company: {
+			type: String,
+			required: [true, 'Please provide company name'],
+			minlength: 2,
+			maxlength: 50,
+		},
+		position: {
+			type: String,
+			required: [true, 'Please provide position'],
+			minlength: 2,
+			maxlength: 100,
+		},
+		status: {
+			type: String,
+			enum: ['interview', 'declined', 'pending'],
+			default: 'pending',
+		},
+		createdBy: {
+			type: mongoose.Types.ObjectId,
+			ref: 'User',
+			required: [true, 'Please provide user'],
+		},
+	},
+	{ timestamps: true }
+)
+
+module.exports = JobSchema
