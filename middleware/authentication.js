b092008ba087279a46964f6bc7be275323d5bf87
Michal Gawik <gawlik.zator@o2.pl>
2024-10-21 12:44:50 +0200

    create authenticationMiddleware

---
+++ middleware/authentication.js
@@ -0,0 +1,12 @@
+const jwt = require('jsonwebtoken')
+const { UnauthenticatedError } = require('../errors')
+
+const authenticationMiddleware = async (req, res, next) => {
+	const authHeader = req.headers.authorization
+
+	// if (!authHeader || !authHeader.startsWith('Bearer ')) {
+	// 	throw new UnauthenticatedError('No token provided')
+	// }
+}
+
+module.exports = authenticationMiddleware
