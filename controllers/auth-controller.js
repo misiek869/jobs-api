0a2f0a8db73bf34d4a12cbe10e412442a1e7e001
Michal Gawik <gawlik.zator@o2.pl>
2024-10-21 09:43:39 +0200

    more validation

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,7 +1,14 @@
 const User = require('../models/user-model')
 const { StatusCodes } = require('http-status-codes')
+const { BadRequestError } = require('../errors')
 
 const register = async (req, res) => {
+	const { name, email, password } = req.body
+
+	if (!name || !email || !password) {
+		throw new BadRequestError('Please provide name, email, password')
+	}
+
 	const user = await User.create({ ...req.body })
 	res.status(StatusCodes.CREATED).json({ user })
 }
