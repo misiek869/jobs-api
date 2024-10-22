19c534eca0ba89bb082b99d1012c155ea44755de
Michal Gawik <gawlik.zator@o2.pl>
2024-10-22 09:55:02 +0200

    create customError handler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,24 +1,33 @@
-const { CustomAPIError } = require('../errors')
-
 const { StatusCodes } = require('http-status-codes')
 
 const errorHandlerMiddleware = (err, req, res, next) => {
 	let customError = {
 		statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-		msg: err.message || 'Something went wrong...',
+		msg: err.message || 'Something went wrong try again later',
 	}
 
-	// if (err instanceof CustomAPIError) {
-	// 	return res.status(err.statusCode).json({ mgs: err.message })
-	// }
+	if (err.name === 'ValidationError') {
+		customError.msg = Object.values(err.errors)
+			.map(item => item.message)
+			.join(',')
+		customError.statusCode = 400
+	}
 
 	if (err.code && err.code === 11000) {
 		customError.msg = `Duplicate value entered for ${Object.keys(
 			err.keyValue
-		)} field. Please choose another value`
+		)} field, please choose another value`
 		customError.statusCode = 400
 	}
-	// return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+
+	if (err.name === 'CastError') {
+		customError.msg = `Can't find item with id: ${err.value}`
+		customError.statusCode = 404
+	}
+
+	console.log(err)
+
+	return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
 	return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
