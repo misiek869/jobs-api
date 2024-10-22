f2359e180a97e3922cacc7419e0fec811f4b92ac
Michal Gawik <gawlik.zator@o2.pl>
2024-10-22 05:26:55 +0200

    create error

--- errors/not-found-error.js
+++ errors/not-found-error.js
@@ -4,6 +4,7 @@ const CustomAPIError = require('./custom-api-error')
 class NotFoundError extends CustomAPIError {
 	constructor(message) {
 		super(message)
+
 		this.statusCode = StatusCodes.NOT_FOUND
 	}
 }
