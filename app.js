7bb3a89992c72d9bd515e009f7f40ee1e53613ad
Michal Gawik <gawlik.zator@o2.pl>
2024-10-22 12:39:28 +0200

    remove rateLimit

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ require('express-async-errors')
 const helmet = require('helmet')
 const cors = require('cors')
 const xss = require('xss-clean')
-const rateLimit = require('express-rate-limit')
 
 const express = require('express')
 const app = express()
@@ -22,13 +21,6 @@ const jobsRouter = require('./routes/jobs-route')
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
-app.set('trust proxy', 1)
-app.use(
-	rateLimit({
-		windowMs: 15 * 60 * 1000, // 15 minutes
-		limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	})
-)
 app.use(express.json())
 app.use(helmet())
 app.use(cors())
